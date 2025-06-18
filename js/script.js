dbab43d8d919102b367232c70281559506a99005
Nebiyu Daniel <nebiyu.daniel@stu.ocsb.ca>
2025-06-18 14:47:53 +0000

    gh

--- js/script.js
+++ js/script.js
@@ -1,5 +1,47 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
+
 //
+
 // Created by: Mr. Coxall
+
 // Created on: Sep 2020
+
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function numberDisplay() {
+
+    let numbers = ""
+
+    let numbers2 = ""
+
+    let userNumMin = parseInt(document.getElementById('minNumber').value)
+
+    let userNumMax = parseInt(document.getElementById('maxNumber').value)
+
+    let counter = userNumMin
+
+    while (counter == userNumMin) {
+
+        numbers = numbers + counter + "<br>"
+
+        counter = counter +1
+
+    }
+
+    while (counter <= userNumMax) {
+
+        numbers2 = numbers2 + counter + "<br>"
+
+        counter = counter +1
+
+    }
+
+
+
+    document.getElementById('min').innerHTML = numbers
+
+    document.getElementById('max').innerHTML = numbers2
+
+}
